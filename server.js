b665397afd3c780d5c1724e7d5fc39675244f1e6
Jérémie Nuel <jeremie.nuel@pm.me>
2023-06-21 15:35:41 +0200

    first

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const app = express()
+
+app.use(express.static('dist'))
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/dist/index.html')
+})
+
+app.listen(port)
+
+console.log(__dirname)
+console.log('Server started at http://localhost:' + port)
